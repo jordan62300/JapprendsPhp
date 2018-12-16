66eb7f115956c67b083c8d8e3a6287369cc6a551
Jordan <jordanfievetpro@outlook.fr>
2018-12-16 11:31:14 +0100

    ajout cours sur function include

---
+++ include/includelink.php
@@ -0,0 +1,3 @@
+<div class="contenue" style="background-color: red;">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt laborum odit totam nisi atque amet, debitis eius, id corrupti provident adipisci, natus molestias. Veritatis, aperiam! Possimus soluta deserunt molestiae aut.
+    </div>
